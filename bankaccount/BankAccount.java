99e7215be2a1c8df9985e12e61702a1e9e379ea0
Dymitr Misiejuk <61292050+DymeqWho@users.noreply.github.com>
2020-11-09 18:35:46 +0100

    added constructors
    added constructors and overload them

--- bankaccount/BankAccount.java
+++ bankaccount/BankAccount.java
@@ -7,6 +7,21 @@ public class BankAccount {
     private long accountNumber;
     private double balance;
 
+    public BankAccount(String customerNamne, String customerEmail, long customerPhoneNumber,
+                       long customerAccountNumber,
+                       double customerBalance) {
+        this.name = customerNamne;
+        this.email = customerEmail;
+        this.phoneNumber = customerPhoneNumber;
+        this.accountNumber = customerAccountNumber;
+        this.balance = customerBalance;
+    }
+
+    public BankAccount() {
+        this("Noname", "no@email.no", 0L, 0L, 0);
+        System.out.println("Empty account. You need do set values.");
+    }
+
     public String getName() {
         return name;
     }
@@ -47,19 +62,21 @@ public class BankAccount {
         this.balance = balance;
     }
 
-    public double depositFounds(double founds){
+    public double depositFounds(double founds) {
         System.out.println("you have now on account: " + (balance + founds) + "$");
         setBalance(balance + founds);
         return getBalance();
     }
 
-    public double withdrawFounds(double withdraw){
-        if(withdraw <= this.balance){
+    public double withdrawFounds(double withdraw) {
+        if (withdraw <= this.balance && withdraw >= 0) {
             setBalance(balance - withdraw);
             System.out.println("balance after withdrawing: " + withdraw + "$" + " is: " + balance + "$");
             return balance;
         }
-        System.out.println("withdraw sum is too big! Your founds are: " + balance + "$ and you were trying to get: " + withdraw + "$");
+        if (withdraw >= this.balance){
+            System.out.println("withdraw sum is too big! Your founds are: " + balance + "$ and you were trying to get: " + withdraw + "$");}
+        else if (withdraw < 0) System.out.println("withdraw sum must be bigger than 0");
         return balance;
     }
 }
--- bankaccount/BankMain.java
+++ bankaccount/BankMain.java
@@ -3,12 +3,22 @@ package bankaccount;
 public class BankMain {
     public static void main(String[] args) {
         BankAccount bankAccount = new BankAccount();
+        System.out.println();
+        System.out.println("bankAccount.getName() = " + bankAccount.getName());
+        System.out.println("bankAccount.getEmail() = " + bankAccount.getEmail());
+        System.out.println("bankAccount.getPhoneNumber() = " + bankAccount.getPhoneNumber());
+        System.out.println("bankAccount.getAccountNumber() = " + bankAccount.getAccountNumber());
+        System.out.println("bankAccount.getBalance() = " + bankAccount.getBalance());
+
+        System.out.println("this was before setting!");
 
         bankAccount.setName("Marian Kakkarekokko");
         bankAccount.setAccountNumber(20_4000_4000_0000_1234L);
         bankAccount.setEmail("marian.kakarekokko@klop.kl");
         bankAccount.setPhoneNumber(70072772);
         bankAccount.setBalance(0);
+
+        System.out.println("after setting values to bankAccount");
 
         System.out.println("Welcome! " + bankAccount.getName());
         System.out.println("Your phone number is " + bankAccount.getPhoneNumber() + " and e-mail: " + bankAccount.getEmail());
@@ -21,5 +31,20 @@ public class BankMain {
         bankAccount.withdrawFounds(9);
         bankAccount.depositFounds(6);
         bankAccount.withdrawFounds(3);
+
+        System.out.println();
+        System.out.println("New account:");
+        BankAccount karynaKakkarekokko = new BankAccount("Karyna Kakkarekokko","karyna.kakkarekokko@kupa.du", 70072772L, 20_2000_4000_5000L, 0);
+        System.out.println("Welcome! " + karynaKakkarekokko.getName());
+        System.out.println("Your phone number is " + karynaKakkarekokko.getPhoneNumber() + " and e-mail: " + karynaKakkarekokko.getEmail());
+        System.out.println("Your bank account number is: " + karynaKakkarekokko.getAccountNumber());
+        System.out.println();
+
+        karynaKakkarekokko.withdrawFounds(-100);
+        karynaKakkarekokko.depositFounds(69);
+        karynaKakkarekokko.withdrawFounds(9);
+        karynaKakkarekokko.withdrawFounds(60);
+        karynaKakkarekokko.depositFounds(88);
+        karynaKakkarekokko.withdrawFounds(44);
     }
 }
